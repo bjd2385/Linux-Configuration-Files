ad08d53f7524fcd85786682bf58b215b3d20fc3e
Brandon Doyle <bjd2385.linux@gmail.com>
2017-07-13 20:03:42 -0400

    Update collect_vimrc.sh

--- collect_vimrc.sh
+++ collect_vimrc.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 # Download my vimrc and install it
 
+mkdir -p ~/.vim/bundle/
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 wget https://raw.githubusercontent.com/bjd2385/vimrc/master/.vimrc -O ~/.vimrc
 vim +PluginInstall +qall
 vim
